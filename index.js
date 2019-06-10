eac04a3da5c3da7247a79781d7f261bf0df11572
LiYang <910706840@qq.com>
2019-06-10 14:21:54 +0800

    change something

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 class Index {
-    constructor() {
+    constructor({xCount = 10, yCount = 20} = {}) {
+        this.randomList = []
+        this.randomOne()
+        this.xCount = xCount
+        this.yCount = yCount
         this.clear()
     }
 
     configController() {
         orderMap[UP] = () => {
-            this.stateHolder.setState2(this.stateHolder.transfer(this.stateHolder.state2, 'rotate'))
-            let state = this.stateHolder.state1.concat(this.stateHolder.state2)
-            this.display.drawRect(state)
+            this.state2Move('rotate')
         }
         orderMap[DOWN] = () => {
-            this.stateHolder.setState2(this.stateHolder.transfer(this.stateHolder.state2, 'down'))
-            let state = this.stateHolder.state1.concat(this.stateHolder.state2)
-            this.display.drawRect(state)
+            this.state2Move('down')
         }
         orderMap[LEFT] = () => {
-            this.stateHolder.setState2(this.stateHolder.transfer(this.stateHolder.state2, 'left'))
-            let state = this.stateHolder.state1.concat(this.stateHolder.state2)
-            this.display.drawRect(state)
+            this.state2Move('left')
         }
         orderMap[RIGHT] = () => {
-            this.stateHolder.setState2(this.stateHolder.transfer(this.stateHolder.state2, 'right'))
-            let state = this.stateHolder.state1.concat(this.stateHolder.state2)
-            this.display.drawRect(state)
+            this.state2Move('right')
         }
 
         document.onkeydown = function (event) {
@@ -33,33 +29,63 @@ class Index {
         }
     }
 
-    randomState2() {
+    state2Move(order) {
+        this.stateHolder.state2Transfer(order)
+        this.display.drawRect(this.stateHolder.state)
+    }
+
+    randomOne() {
         let index = Math.floor(Math.random() * dataMap.length)
+        this.randomList.push(index)
+    }
+
+    randomState2() {
+        this.randomOne()
+        let index = this.randomList.shift()
         let data = Util.copy(dataMap[index])
         data = data.map((item) => {
-            item.x += 10
-            item.y += 20
+            item.x += Math.ceil(this.xCount / 2)
+            item.y += this.yCount
             return item
         })
-        let state1 = Util.copy(this.stateHolder.state1)
-        this.clear()
         this.stateHolder.setState2(data)
-        this.stateHolder.setState1(state1)
     }
 
     haveInterval() {
         this.timer = setInterval(() => {
-            let state = this.stateHolder.state1.concat(this.stateHolder.state2)
-            console.log('111', state)
-            this.display.drawRect(state)
-            if (!this.stopTag) {
-                this.stateHolder.setState2(this.stateHolder.transfer(this.stateHolder.state2, 'down'))
+            this.display.drawRect(this.stateHolder.state)
+            this.display1.drawRect(dataMap[this.randomList[0]])
+            document.getElementById('area3').innerText = this.stateHolder.score
+            if (this.stopTag) {
+                this.stateHolder.state2Transfer('down')
             }
-        }, 2000)
+        }, 500)
+    }
+
+    firstStart() {
+        this.randomState2()
+        this.haveInterval()
+    }
+
+    set stopTag(value) {
+        this.stateHolder.stopTag = value
+        this._stopTag = value
+    }
+
+    get stopTag() {
+        return this._stopTag
+    }
+
+    toggle() {
+        if (!this.stopTag) {
+            this.start()
+        } else {
+            this.stop()
+        }
     }
 
     start() {
-        this.randomState2()
+        this.stopTag = true
         this.haveInterval()
     }
 
@@ -69,13 +95,19 @@ class Index {
     }
 
     clear() {
-        this.display = new Display({xCount: 20, yCount: 20})
-        this.stateHolder = new StateHolder()
+        this.display = new Display({xCount: this.xCount, yCount: this.yCount})
+        this.display1 = new Display({id: 'area2', xCount: 4, yCount: 4})
+        this.stateHolder = new StateHolder({xCount: this.xCount, yCount: this.yCount})
         this.timer = null
-        this.stopTag = false
+        this.stopTag = true//true进行中 false暂停中
 
-        this.stateHolder.setListener(() => {
+        this.stateHolder.setState2Listener(() => {
             this.randomState2()
+        })
+
+        this.stateHolder.addDeathListener(() => {
+            this.stop()
+            alert('失败了')
         })
 
         this.configController()
@@ -83,15 +115,173 @@ class Index {
 
     restart() {
         this.clear()
-        this.start()
+        this.firstStart()
     }
 
-    success() {
+}
 
+class StateHolder {
+
+    constructor({xCount = 20, yCount = 20} = {}) {
+        this.score = 0
+        this.deathHappen = []
+        this.stopTag = true
+        this.xCount = xCount
+        this.yCount = yCount
+        this.state1 = []
+        for (let i = 0; i < this.xCount; i++) {
+            this.state1.push({x: i, y: -1})
+        }
+        for (let i = 0; i < this.yCount; i++) {
+            this.state1.push({x: -1, y: i})
+            this.state1.push({x: this.xCount, y: i})
+        }
+        this.state2 = []
     }
 
-    fail() {
+    get state() {
+        return this.state1.concat(this.state2)
+    }
 
+    setState2Listener(fun) {
+        this.newState2Event = fun
+    }
+
+    addDeathListener(fun) {
+        this.deathHappen.push(fun)
+    }
+
+    setState2(data) {
+        this.state2 = data
+    }
+
+    checkDeath() {
+        for (let item of this.state1) {
+            if (item.y >= this.yCount) {
+                return true
+            }
+        }
+        return false
+    }
+
+    add() {
+        this.state1 = this.state
+        this.state2.length = 0
+    }
+
+    delete(arr) {
+        for (let it of arr) {
+            this.state1 = this.state1.filter(item => {
+                return item.y !== it
+            })
+            let data = this.state1.filter(item => {
+                return item.y > it
+            })
+            transferMap['down'](data)
+        }
+        this.score += arr.length
+    }
+
+    checkDelete() {//连成了一行
+        let result = []
+
+        for (let i = 0; i < this.yCount; i++) {
+            let count = 0
+            for (let item2 of this.state1) {
+                if (i === item2.y) {
+                    count++
+                }
+            }
+            if (count === this.xCount + 2) {
+                result.push(i)
+            }
+        }
+
+        return result
+    }
+
+    checkState2Transfer() {//发生碰撞
+        for (let item of this.state1) {
+            if (Util.isIncludes(this.state2, item)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    state2Transfer(name) {
+        if (!this.stopTag) {
+            return
+        }
+        let fun = transferMap[name]
+        let temp = Util.copy(this.state2)
+        fun(this.state2)
+        if (this.checkState2Transfer()) {
+            this.setState2(temp)
+            if (name === 'down') {
+                this.add()
+                let deleteArr = this.checkDelete()
+                console.log(deleteArr)
+                if (deleteArr.length > 0) {
+                    this.delete(deleteArr)
+                }
+                if (this.checkDeath()) {
+                    for (let fun of this.deathHappen) {
+                        fun()
+                    }
+                }
+                this.newState2Event()
+            }
+        }
+    }
+}
+
+class Display {
+
+    constructor({id = 'area1', width = 10, height = width, xCount = 4, yCount = 4, color1 = 'pink', color2 = 'black'} = {}) {
+        this.id = id
+        this.width = width
+        this.height = height
+        this.xCount = xCount
+        this.yCount = yCount
+        this.color1 = color1
+        this.color2 = color2
+    }
+
+    clearRect() {
+        let dom = document.getElementById(this.id)
+        dom.innerHTML = ''
+    }
+
+    drawRect(data) {
+        this.clearRect()
+        let borderWidth = 1
+        let dom = document.getElementById(this.id)
+        let domWidth = this.xCount * (this.width + borderWidth * 2)
+        dom.style.width = `${domWidth}px`
+        dom.style.display = 'flex'
+        dom.style.flexWrap = 'wrap-reverse'
+
+        for (let i = 0; i < this.yCount; i++) {
+            for (let j = 0; j < this.xCount; j++) {
+                if (Util.isIncludes(data, {x: j, y: i})) {
+                    this.border = `${borderWidth}px solid ${this.color2}`
+                } else {
+                    this.border = `${borderWidth}px solid ${this.color1}`
+                }
+                let smallRect = this.createSmallRect()
+                dom.appendChild(smallRect)
+            }
+
+        }
+    }
+
+    createSmallRect() {
+        let smallRect = document.createElement('div')
+        smallRect.style.width = this.width + 'px'
+        smallRect.style.height = this.height + 'px'
+        smallRect.style.border = this.border
+        return smallRect
     }
 }
 
@@ -121,128 +311,20 @@ class Util {
     }
 }
 
-class StateHolder {
-
-    constructor({state1 = [], state2 = [], width = 20} = {}) {
-        this.state1 = state1
-        this.state2 = state2
-        this.width = width
-    }
-
-    setListener(fun) {
-        this.newState2Event = fun
-    }
-
-    setState1(data) {
-        this.state1 = data
-    }
-
-    setState2(data) {
-        this.state2 = data
-    }
-
-    state1TopBorder() {
-        let result = []
-        let temp = Util.copy(this.state1)
-        let flag = false
-        for (let item of temp) {
-            for (let i = 0; i < result.length; i++) {
-                let it = result[i]
-                if (it.x === item.x) {
-                    flag = true
-                    if (it.y < item.y) {
-                        result[i] = item
-                    }
-                }
-            }
-            if (!flag) {
-                result.push(item)
-            }
-        }
-        return result
-    }
-
-    state2BottomBorder() {
-        let result = []
-        let temp = Util.copy(this.state2)
-        let flag = false
-        for (let item of temp) {
-            for (let i = 0; i < result.length; i++) {
-                let it = result[i]
-                if (it.x === item.x) {
-                    flag = true
-                    if (it.y > item.y) {
-                        result[i] = item
-                    }
-                }
-            }
-            if (!flag) {
-                result.push(item)
-            }
-        }
-        return result
-    }
-
-    add() {
-        this.state1 = this.state1.concat(this.state2)
-    }
-
-    delete(data) {
-        this.transfer(this.state1, 'down')
-    }
-
-    touchAdd() {
-        let state1TopData = this.state1TopBorder()
-        let state2BottomData = this.state2BottomBorder()
-        for (let i = 0; i < this.width; i++) {
-            state1TopData.push({x: i, y: -1})
-        }
-        for (let state2Item of state2BottomData) {
-            for (let state1Item of state1TopData) {
-                let temp = Util.copy(state1Item)
-                temp.y += 1
-                if (Point.prototype.equals.call(state2Item, temp)) {
-                    // 发生了
-                    this.add()
-                    this.newState2Event()
-                    return
-                }
-            }
-        }
-    }
-
-    touchDelete() {
-
-    }
-
-    transfer(data, name) {
-        let fun = transferMap[name]
-        let result = fun(data)
-        this.touchAdd()
-        return result
-    }
-}
-
 let transferMap = {
     down: function (data) {
-        return data.map((item) => {
-            item = Util.copy(item)
+        data.forEach((item) => {
             item.y -= 1
-            return item
         })
     },
     left: function (data) {
-        return data.map((item) => {
-            item = Util.copy(item)
+        data.forEach((item) => {
             item.x -= 1
-            return item
         })
     },
     right: function (data) {
-        return data.map((item) => {
-            item = Util.copy(item)
+        data.forEach((item) => {
             item.x += 1
-            return item
         })
     },
     rotate: function (data) {
@@ -257,12 +339,10 @@ let transferMap = {
             y: totalPoint.y / data.length
         }
 
-        return data.map((item) => {
+        data.forEach((item) => {
             let temp = Util.copy(item)
-            let temp1 = Util.copy(item)
-            temp.x = Math.floor(Math.ceil(temp1.y - center.y) + center.x)
-            temp.y = Math.floor(Math.ceil(-(temp1.x - center.x)) + center.y)
-            return temp
+            item.x = Math.floor(Math.ceil(temp.y - center.y) + center.x)
+            item.y = Math.floor(Math.ceil(-(temp.x - center.x)) + center.y)
         })
     },
 }
@@ -285,51 +365,3 @@ let dataMap = [
 let [UP, DOWN, LEFT, RIGHT] = ['38', '40', '37', '39']
 let orderMap = {}
 
-class Display {
-
-    constructor({id = 'area1', width = 10, height = width, xCount = 4, yCount = 4, color1 = 'pink', color2 = 'black'} = {}) {
-        this.id = id
-        this.width = width
-        this.height = height
-        this.xCount = xCount
-        this.yCount = yCount
-        this.color1 = color1
-        this.color2 = color2
-    }
-
-    clearRect() {
-        let area1 = document.getElementById(this.id)
-        area1.innerHTML = ''
-    }
-
-    drawRect(data) {
-        this.clearRect()
-        let borderWidth = 1
-        let area1 = document.getElementById(this.id)
-        let area1Width = this.xCount * (this.width + borderWidth * 2)
-        area1.style.width = `${area1Width}px`
-        area1.style.display = 'flex'
-        area1.style.flexWrap = 'wrap-reverse'
-
-        for (let i = 0; i < this.yCount; i++) {
-            for (let j = 0; j < this.xCount; j++) {
-                if (Util.isIncludes(data, {x: j, y: i})) {
-                    this.border = `${borderWidth}px solid ${this.color2}`
-                } else {
-                    this.border = `${borderWidth}px solid ${this.color1}`
-                }
-                let smallRect = this.createSmallRect()
-                area1.appendChild(smallRect)
-            }
-
-        }
-    }
-
-    createSmallRect() {
-        let smallRect = document.createElement('div')
-        smallRect.style.width = this.width + 'px'
-        smallRect.style.height = this.height + 'px'
-        smallRect.style.border = this.border
-        return smallRect
-    }
-}
